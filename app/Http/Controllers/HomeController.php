69b00a9aa48f29357e063ada2092266f1b52e4ce
zakiamarrit <zakiamarrit@gmail.com>
2022-12-03 21:14:45 +0700

    join table

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,8 +13,12 @@ class HomeController extends Controller
         $datas = DB::select('select * from admin');
         $datas2 = DB::select('select * from histori_stok_obat');
         $datas3 = DB::select('select * from obat');
+        $datas4 = DB::table('histori_stok_obat')
+                    ->join('obat', 'obat.id_obat', '=', 'histori_stok_obat.id_obat')
+                    ->get();
 
         return view('home')
+            ->with('datas4', $datas4)
             ->with('datas3', $datas3)
             ->with('datas2', $datas2)
             ->with('datas', $datas);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,38 @@
 <h1 class="mt-4">Database</h1>
 
 <div class="card mb-2">
+    <div class="p-3 mb-2 bg-warning text-dark">Join Table</div>
+    <table class="table table-hover mt-2">
+        <thead>
+        <tr>
+            <th>ID Histori Obat</th>
+            <th>Nama Obat</th>
+            <th>Tipe Obat</th>
+            <th>ID Obat</th>
+            <th>ID Unit Kerja</th>
+            <th>Tanggal Stok</th>
+            <th>Tanggal Kadaluarsa</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach ($datas4 as $data)
+                <tr>
+                    <td>{{ $data->id_histori_stok_obat}}</td>
+                    <td>{{ $data->nama_obat }}</td>
+                    <td>{{ $data->tipe_obat }}</td>
+                    <td>{{ $data->id_obat }}</td>
+                    <td>{{ $data->id_unit_kerja}}</td>
+                    <td>{{ $data->tgl_stok }}</td>
+                    <td>{{ $data->tgl_kadaluarsa }}</td>
+                </tr>
+            @endforeach
+    
+        </tbody>
+    </table>
+    
+</div>
+
+<div class="card mb-2">
     <div class="p-3 mb-2 bg-info text-dark">Riwayat</div>
     <table class="table table-hover mt-2">
     <thead>
@@ -80,6 +112,7 @@
         </tbody>
     </table>
 </div>
+
 
 
 
